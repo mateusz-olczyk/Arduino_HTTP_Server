8581a1f7d9a7f1d15cec7aa89e47a31911cdc3a3
Michał <mgrzesik7@gmail.com>
2017-12-29 23:30:59 +0100

    Added posibility to change INCREMENTAL_VALUE by HTTP request

--- Webpage.ino
+++ Webpage.ino
@@ -1,9 +1,15 @@
-const PROGMEM char prgstrHTTPHeader[] = "HTTP/1.1 200 OK\nContent-Type: text/html\nConnection: close\nRefresh: 5\n\n";
-const PROGMEM char prgstrPageBegin[] = "<!doctype html>\n<html>\n<head>\n<title>Scatter Chart</title>\n<script src=\"http://www.chartjs.org/dist/2.7.1/Chart.bundle.js\"></script>\n<style>\ncanvas{\n-moz-user-select: none;\n-webkit-user-select: none;\n-ms-user-select: none;\n}\ndiv{\nwidth: 50%;\nmargin: 0 auto;\n}\n</style>\n</head>\n<body>\n<div style=\"width:75%;\">\n<canvas id=\"canvas\"></canvas>\n</div>\n<script>\nvar config = {\ntype: \"scatter\",\ndata: {\ndatasets: [{\nbackgroundColor: \"#1b5df6\",\nborderColor: \"#1b5df6\",\nshowLine: true,\ndata: [";
-const PROGMEM char prgstrPageEnd[] = "],\nfill: false\n}]\n},\noptions: {\nresponsive: true,\nlegend: {\ndisplay: false\n},\nanimation: {\nduration: 0\n},\nresponsiveAnimationDuration: 0,\ntitle:{\ndisplay:true,\ntext:'Chart.js Scatter Chart with lines'\n},\nscales: {\nxAxes: [{\nscaleLabel: {\ndisplay: true,\nlabelString: 'seconds'\n}\n}],\nyAxes: [{\nscaleLabel: {\ndisplay: true,\nlabelString: 'value'\n}\n}]\n}\n}\n};\nwindow.onload = function() {\nvar ctx = document.getElementById(\"canvas\").getContext(\"2d\");\nwindow.myLine = new Chart(ctx, config);\n};\n</script>\n</body>\n</html>";
+const PROGMEM char prgstrHTTPHeader[] = "HTTP/1.1 200 OK\nContent-Type: text/html\nConnection: close\nRefresh: 150\n\n";
+const PROGMEM int maxIncrementValueSize = 10;
+const char newIncValueRequest[] = "NEW_LITRES_VALUE";
+const PROGMEM int newIncValueRequestSize = 16;
+const PROGMEM int maxHeaderSize = 6 + newIncValueRequestSize + maxIncrementValueSize; 
+const PROGMEM char prgstrPageBegin[] = "<!doctype html>\n<html>\n<head>\n<title>Scatter Chart</title>\n<script src=\"http://www.chartjs.org/dist/2.7.1/Chart.bundle.js\"></script>\n<style>\ncanvas{\n-moz-user-select: none;\n-webkit-user-select: none;\n-ms-user-select: none;\n}\ndiv{\nwidth: 50%;\nmargin: 0 auto;\n}\n</style>\n<script>function sendLitresAmount() {\nvar request = new XMLHttpRequest();\nvar counter_litres = document.getElementById(\"counter_litres_input\").value;\nrequest.open(\"POST\", \"NEW_LITRES_VALUE\" + counter_litres, true);\nrequest.send(counter_litres);\ndocument.getElementById(\"counter_litres_input\").value=\"\";}\n</script>\n</head>\n<body>\n<div style=\"width:75%;\">\n<canvas id=\"canvas\"></canvas>\n<div><p>Liczba litrow na pojedynczy sygnal: <input type=\"text\" id=\"counter_litres_input\"><button type=\"button\" onclick=\"sendLitresAmount()\">Wyslij</button></p></div>\n<script>\nvar config = {\ntype: \"scatter\",\ndata: {\ndatasets: [{\nbackgroundColor: \"#1b5df6\",\nborderColor: \"#1b5df6\",\nshowLine: true,\ndata: [";
+const PROGMEM char prgstrPageEnd[] = "],\nfill: false\n}]\n},\noptions: {\nresponsive: true,\nlegend: {\ndisplay: false\n},\nanimation: {\nduration: 0\n},\nresponsiveAnimationDuration: 0,\ntitle:{\ndisplay:true,\ntext:'Chart.js Scatter Chart with lines'\n},\nscales: {\nxAxes: [{\nticks: { reverse: true\n},\nscaleLabel: {\ndisplay: true,\nlabelString: 'seconds'\n}\n}],\nyAxes: [{\nscaleLabel: {\ndisplay: true,\nlabelString: 'value'\n}\n}]\n}\n}\n};\nwindow.onload = function() {\nvar ctx = document.getElementById(\"canvas\").getContext(\"2d\");\nwindow.myLine = new Chart(ctx, config);\n};\n</script>\n</body>\n</html>";
 
-//const PROGMEM char progmem_string[] = "<!doctype html>\n<html>\n<head>\n<title>Scatter Chart</title>\n<script src=\"http://www.chartjs.org/dist/2.7.1/Chart.bundle.js\"></script>\n<style>\ncanvas{\n-moz-user-select: none;\n-webkit-user-select: none;\n-ms-user-select: none;\n}\ndiv{\nwidth: 50%;\nmargin: 0 auto;\n}\n</style>\n</head>\n<body>\n<div style=\"width:75%;\">\n<canvas id=\"canvas\"></canvas>\n</div>\n<script>\nvar config = {\ntype: \"scatter\",\ndata: {\ndatasets: [{\nbackgroundColor: \"#1b5df6\",\nborderColor: \"#1b5df6\",\nshowLine: true,\ndata: [{\nx: 0.7,\ny: 5.6\n}, {\nx: 5.7,\ny: 10.7\n}, {\nx: 8.7,\ny: 5.7\n}, {\nx: 15.7,\ny: 0.7\n}],\nfill: false\n}]\n},\noptions: {\nresponsive: true,\nlegend: {\ndisplay: false\n},\nanimation: {\nduration: 0\n},\nresponsiveAnimationDuration: 0,\ntitle:{\ndisplay:true,\ntext:'Chart.js Scatter Chart with lines'\n},\nscales: {\nxAxes: [{\nscaleLabel: {\ndisplay: true,\nlabelString: 'x axis'\n}\n}],\nyAxes: [{\nscaleLabel: {\ndisplay: true,\nlabelString: 'y axis'\n}\n}]\n}\n}\n};\nwindow.onload = function() {\nvar ctx = document.getElementById(\"canvas\").getContext(\"2d\");\nwindow.myLine = new Chart(ctx, config);\n};\n</script>\n</body>\n</html>";
 extern DataBuffer dataBuffer;
+extern double INCREMENT_VALUE;
+
+char newIncrementValue[maxIncrementValueSize] = {"0"};
 
 void printProgmemString(EthernetClient & client, const PROGMEM char string[]) {
   for (int i = 0; i < strlen_P(string); i++)
@@ -20,15 +26,24 @@ void show_webpage(EthernetClient & client) {
 void handle_client(EthernetClient & client) {
   if (client) {
     Serial.println("new client");
+    int currentIndex=-1;
+    boolean isIncValuePostRequest = false;
     // an http request ends with a blank line
     boolean currentLineIsBlank = true;
     while (client.connected()) {
       if (client.available()) {
         char c = client.read();
+        currentIndex++;
         Serial.write(c);
         // if you've gotten to the end of the line (received a newline
         // character) and the line is blank, the http request has ended,
         // so you can send a reply
+        if (currentIndex <maxHeaderSize){
+           if(c == newIncValueRequest[0]){
+              parseHeader(client, currentIndex);
+           }
+        }
+        
         if (c == '\n' && currentLineIsBlank) {
           show_webpage(client);
           break;
@@ -48,5 +63,49 @@ void handle_client(EthernetClient & client) {
     client.stop();
     Serial.println("client disconnected");
   }
+}
+
+double getValue(EthernetClient & client, int & currentIndex){
+  for (int i=0; i<maxIncrementValueSize; i++){
+    newIncrementValue[i]='\0';  
+  }
+  char c;
+  int i=0;
+  while(true){
+    c = client.read();
+    Serial.write(c);
+    currentIndex++;
+    if(i < maxIncrementValueSize-1  && ((c>='0' && c<='9') || c == '.' )){
+      newIncrementValue[i]=c;
+    }else{
+      if(i==0){
+        return 1.0;
+      }
+      break;
+    }
+    i++;
+  }
+  return atof(newIncrementValue);
+}
+
+void setNewIncValueNumber(EthernetClient & client, int & currentIndex){
+  INCREMENT_VALUE = getValue(client,currentIndex);
+  Serial.print("New INCREMENT_VALUE = ");Serial.println(INCREMENT_VALUE);
+}
+
+void parseHeader(EthernetClient & client, int & currentIndex){
+  int patternIndex = 1;
+  char c;
+  while (patternIndex < newIncValueRequestSize){
+    c = client.read();
+    Serial.write(c);
+    currentIndex++;
+    if(c != newIncValueRequest[patternIndex]){
+      return;
+    }
+    patternIndex++;
+  }
+
+  setNewIncValueNumber(client, currentIndex);
 }
 
